a0a8fc22f5f5b184c3ef7b5231751cd5692d5b66
GoninDS <luisdi2509@gmail.com>
2024-09-17 17:07:19 -0600

    Implementation without testing

--- src/hopcroft.py
+++ src/hopcroft.py
@@ -8,14 +8,38 @@ def prepare_initial_state(states, accepting_states):
             non_accepting.append(i)
 
     new_states.add(frozenset(accepting))
-    new_states.add(frozenset(non_accepting))
+    new_states.add(frozenset(non_accepting))   
     return new_states
+
+def split(current_set, alphabet, transitions):
+    hash_table = {}
+    result = set()
+
+    for state in current_set:
+        key = ''
+        for action in alphabet:
+            key += str(transitions[state][action])
+        if key not in hash_table:
+            hash_table[key] = []
+        hash_table[key].append(state)
+    
+    values = hash_table.values()
+    for current in values:
+        result.add(frozenset(current))
+
+    return result
 
 def hopcroft(states, alphabet, accepting_states, transitions):
     new_states = prepare_initial_state(states, accepting_states)
-    print(new_states)
-    return new_states
+    current_states = None
 
+    while (current_states != new_states):
+        current_states = new_states.copy()
+        new_states = set()
+        for current_set in current_states:
+            new_states = new_states | split(current_set, alphabet, transitions)
+
+    return new_states
 
 
 states = {0, 1, 2, 3, 4, 5}
